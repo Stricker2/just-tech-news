f0e18e0abf06cd74f191ad10dc471119c3a05f42
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-07-21 19:28:26 -0400

    add login route for authentication

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // creat User Model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and config
 User.init(
@@ -44,17 +50,24 @@ User.init(
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        
         // table configuration  (https://sequelize.org/v5/manual/models-definition.html#configuration)
-
-        // pass in imported sequelize connection (direct connection to our database)
         sequelize,
-        // don't automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
-        // dont pluralize name of database table
         freezeTableName: true,
-        // use underscores instead of camel casing
         underscored: true,
-        // make it so our model name stays lowercase
         modelName: 'user'
     }
 );
